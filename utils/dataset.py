f5c0e8340e739f3654ed5b1779f30079828c9836
a170811 <a170811@gmail.com>
2020-06-19 01:29:35 +0800

    refine

--- utils/dataset.py
+++ utils/dataset.py
@@ -2,7 +2,6 @@ import pickle
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
 
 
 def build_data(path):
@@ -12,32 +11,10 @@ def build_data(path):
     pos['ans'] = 1
     neg['ans'] = 0
 
-    print('pos\n', pos.head(5))
-    print('neg\n', neg.head(5))
-
     pos, neg = pos.to_numpy(), neg.to_numpy()
     data = np.concatenate([pos, neg], axis=0) # (389891, 3)
 
-    # x1 pad to 31 as x2
-    pad = lambda x, n: x + n*'0'
-    y = data[:, 2]
-    x = np.array([[onehot(pad(x1, 10)), onehot(x2)] for x1, x2 in data[:, :2]])
-    print(f'x: {x.shape}')
-    print(f'y: {y.shape}')
-
-    return x, y
-
-
-def onehot(seq):
-
-    transform = {
-        'A': [1, 0, 0, 0],
-        'T': [0, 1, 0, 0],
-        'C': [0, 0, 1, 0],
-        'G': [0, 0, 0, 1],
-        '0': [0, 0, 0, 0] # padding
-    }
-    return [transform[c] for c in seq]
+    return data[:, :2], data[:, 2]
 
 
 def split(x, y, ratio=[0.8, 0.1, 0.1], seed=0):
@@ -53,3 +30,4 @@ def split(x, y, ratio=[0.8, 0.1, 0.1], seed=0):
 if '__main__' == __name__:
 
     path = '../data/raw/'
+    x, y = build_data(path)
