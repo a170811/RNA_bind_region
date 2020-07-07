e56d4edfc28b70946a819468333bd3cac8e5e060
a170811 <a170811@gmail.com>
2020-07-08 00:46:24 +0800

    add MCC/Specificity

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from functools import reduce
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import tensorflow_addons as tfa
 
 from utils.dataset import build_data, split
 
@@ -44,6 +43,76 @@ class F1_score(tf.keras.metrics.Metric):# {{{
     self.tp.assign(0.)
     self.fp.assign(0.)
     self.fn.assign(0.)
+# }}}
+
+class Specificity(tf.keras.metrics.Metric):# {{{
+  def __init__(self, thresholds=0.5, name='specificity', **kwargs):
+    super(Specificity, self).__init__(name=name, **kwargs)
+    self.specificity = self.add_weight(name='specificity', initializer='zeros')
+    self.tn = self.add_weight(name='tn', initializer='zeros')
+    self.fp = self.add_weight(name='fp', initializer='zeros')
+    self.thresholds=thresholds
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    min_delta=1e-6
+    y_pred=tf.cast(tf.where(y_pred>self.thresholds,1,0),tf.int8)
+    y_true=tf.cast(y_true,tf.int8)
+
+    fp=tf.math.count_nonzero(y_pred*(1-y_true),dtype=tf.float32)
+    tn=tf.math.count_nonzero((1-y_pred)*(1-y_true),dtype=tf.float32)
+
+    self.fp.assign_add(fp)
+    self.tn.assign_add(tn)
+
+    self.specificity.assign(self.tn/(self.fp+self.tn+min_delta))
+
+  def result(self):
+    return self.specificity
+
+  def reset_states(self):
+    # The state of the metric will be reset at the start of each epoch.
+    self.specificity.assign(0.)
+    self.fp.assign(0.)
+    self.tn.assign(0.)
+# }}}
+
+class MCC(tf.keras.metrics.Metric):# {{{
+  def __init__(self, thresholds=0.5, name='mcc', **kwargs):
+    super(MCC, self).__init__(name=name, **kwargs)
+    self.mcc = self.add_weight(name='mcc', initializer='zeros')
+    self.tp = self.add_weight(name='tp', initializer='zeros')
+    self.fp = self.add_weight(name='fp', initializer='zeros')
+    self.fn = self.add_weight(name='fn', initializer='zeros')
+    self.tn = self.add_weight(name='tn', initializer='zeros')
+    self.thresholds=thresholds
+
+  def update_state(self, y_true, y_pred, sample_weight=None):
+    min_delta=1e-6
+    y_pred=tf.cast(tf.where(y_pred>self.thresholds,1,0),tf.int8)
+    y_true=tf.cast(y_true,tf.int8)
+
+    tp=tf.math.count_nonzero(y_pred*y_true,dtype=tf.float32)
+    fp=tf.math.count_nonzero(y_pred*(1-y_true),dtype=tf.float32)
+    fn=tf.math.count_nonzero((1-y_pred)*y_true,dtype=tf.float32)
+    tn=tf.math.count_nonzero((1-y_pred)*(1-y_true),dtype=tf.float32)
+
+    self.tp.assign_add(tp)
+    self.fp.assign_add(fp)
+    self.fn.assign_add(fn)
+    self.tn.assign_add(tn)
+
+    self.mcc.assign((self.tp*self.tn-self.fp*self.fn)/(((self.tp+self.fp)*(self.tp+self.fn)*(self.tn+self.fp)*(self.tn+self.fn))**(1/2)+min_delta))
+
+  def result(self):
+    return self.mcc
+
+  def reset_states(self):
+    # The state of the metric will be reset at the start of each epoch.
+    self.mcc.assign(0.)
+    self.tp.assign(0.)
+    self.fp.assign(0.)
+    self.fn.assign(0.)
+    self.tn.assign(0.)
 # }}}
 
 def build_conv_block(shape):# {{{
@@ -109,11 +178,14 @@ def build_model():# {{{
     x = tf.keras.layers.Dense(1, activation='sigmoid')(x)
     model = tf.keras.Model(inputs=[inputs_pi, inputs_m], outputs=x)
     model.summary()
-    auc = tf.keras.metrics.AUC()
-    f1 = F1_score()
+    accuracy = tf.keras.metrics.BinaryAccuracy(name='acc')
     precision = tf.keras.metrics.Precision()
     recall = tf.keras.metrics.Recall()
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', auc, f1, precision, recall])
+    specificity = Specificity()
+    f1 = F1_score()
+    auc = tf.keras.metrics.AUC()
+    mcc = MCC()
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[accuracy, precision, recall, specificity, f1, auc, mcc])
     return model
 # }}}
 
@@ -192,9 +264,9 @@ def train_and_eval(model_name, seed=0, save=True):# {{{
 if '__main__' == __name__:
 
 
-    # res = train_and_eval('base_cnn', save=False)
-    # print(res)
-    # exit()
+    res = train_and_eval('base_cnn', save=False)
+    print(res)
+    exit()
 
     # v1: base_cnn
     # v2: base_cnn_drop0.4 #2020/07/03
